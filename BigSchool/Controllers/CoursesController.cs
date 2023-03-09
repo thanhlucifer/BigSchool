e82ca913f3a05d31dfb031561790542facac97c4
Pham Van Thanh <88926531+thanhlucifer@users.noreply.github.com>
2023-03-09 15:33:22 +0700

    next

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -17,6 +17,7 @@ namespace BigSchool.Controllers
         {
             _dbContext = new ApplicationDbContext();
         }
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
